ecdc9c6b21d2bc00764e975b260cba47489826e7
feliperyo <ryofeliperyo@gmail.com>
2024-04-22 13:38:45 -0300

    primeiro commit

---
+++ src/database/index.js
@@ -0,0 +1,20 @@
+import { Sequelize } from 'sequelize'
+
+import configDataBase from '../config/database'
+
+import User from '../app/models/User'
+import Product from '../app/models/Product'
+
+const models = [User, Product]
+
+class DataBase {
+    constructor() {
+        this.init()
+    }
+    init() {
+        this.connection = new Sequelize(configDataBase)
+        models.map((model) => model.init(this.connection))
+    }
+}
+
+export default new DataBase()
