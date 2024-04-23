c9c1304ed714091dadee123fc4fc5c11fb2c5251
feliperyo <ryofeliperyo@gmail.com>
2024-04-23 09:09:07 -0300

    configurando token JWT

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import multer from 'multer'
 import multerConfig from './config/multer'
+import authMiddleware from './middlewares/auth'
 
 import UserContoller from './app/controllers/UserController'
 import SessionController from './app/controllers/SessionController'
@@ -12,6 +13,8 @@ const upload = multer(multerConfig)
 
 routes.post('/users', UserContoller.store)
 routes.post('/session', SessionController.store)
+
+routes.use(authMiddleware)
 routes.post('/products', upload.single('file'), ProductController.store)
 routes.get('/products', ProductController.index)
 
